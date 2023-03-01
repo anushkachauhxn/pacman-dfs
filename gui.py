2f0b56a28448ef65a4725ed57498c8170f2d51e0
Anushka Chauhan <anushkachauhan1503@gmail.com>
2023-03-02 01:29:00 +0530

    GUI: Dynamic Input

--- gui.py
+++ gui.py
@@ -2,70 +2,57 @@ from tkinter import Tk, Canvas, Frame, BOTH
 import time
 
 
-grid = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0],
-    [0, 1, 0, 0, 0, 1, 0],
-    [0, 1, 0, 2, 0, 1, 0],
-    [0, 1, 1, 1, 0, 1, 0],
-    [0, 1, 0, 0, 0, 1, 0],
-    [0, 1, 1, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-]
-n = len(grid)  # n=8
-m = len(grid[0])  # n=7
-pacmanPath = [[6, 1], [5, 1], [4, 1], [3, 1], [2, 1], [1, 1],
-              [1, 2], [1, 3], [1, 4], [1, 5],
-              [2, 5], [3, 5], [4, 5], [5, 5], [6, 5],
-              [6, 4], [6, 3], [6, 2],
-              [4, 2], [4, 3], [3, 3]]
-
-
 class Board(Frame):
-    def __init__(self):
+    def __init__(self, grid, pacmanPath):
         super().__init__()
+        self.grid = grid
+        self.pacmanPath = pacmanPath
         self.initUI()
 
     def initUI(self):
+        n = len(self.grid)
+        m = len(self.grid[0])
+
         self.master.title("Pacman | DFS")
         self.master.geometry(f"{50*m}x{50*n}")
         self.pack(fill=BOTH, expand=1)
-        canvas = Canvas(self)
+        self.canvas = Canvas(self)
 
-        create_grid(canvas, n, m)  # grid maze
+        create_grid(self.canvas, self.grid)  # grid maze
 
         progress = 0
-        while (progress != len(pacmanPath)-1):
+        while (progress != len(self.pacmanPath)-1):
             pacman = create_pacman(
-                canvas, pacmanPath[progress][0], pacmanPath[progress][1])
-            canvas.pack(fill=BOTH, expand=1)
+                self.canvas, self.pacmanPath[progress][0], self.pacmanPath[progress][1])
+            self.canvas.pack(fill=BOTH, expand=1)
             self.master.update()
             time.sleep(0.5)
 
-            for i in range(progress+1, len(pacmanPath)):
-                print(i)
-                x = pacmanPath[i][0]
-                px = pacmanPath[i-1][0]
-                y = pacmanPath[i][1]
-                py = pacmanPath[i-1][1]
+            for i in range(progress+1, len(self.pacmanPath)):
+                x = self.pacmanPath[i][0]
+                px = self.pacmanPath[i-1][0]
+                y = self.pacmanPath[i][1]
+                py = self.pacmanPath[i-1][1]
 
                 if (x == px-1):
-                    movePacman(canvas, 'up')  # x dec
+                    movePacman(self.canvas, 'up')  # x dec
                 elif (x == px+1):
-                    movePacman(canvas, 'down')  # x inc
+                    movePacman(self.canvas, 'down')  # x inc
                 elif (y == py-1):
-                    movePacman(canvas, 'left')  # y dec
+                    movePacman(self.canvas, 'left')  # y dec
                 elif (y == py+1):
-                    movePacman(canvas, 'right')  # y inc
+                    movePacman(self.canvas, 'right')  # y inc
                 else:
-                    canvas.delete(pacman)
+                    self.canvas.delete(pacman)
                     break
                 self.master.update()
                 time.sleep(0.5)
             progress = i
 
 
-def create_grid(canvas, n, m):
+def create_grid(canvas, grid):
+    n = len(grid)
+    m = len(grid[0])
     # create a grid with n rows and m columns
     for i in range(0, n):
         for j in range(0, m):
@@ -98,8 +85,24 @@ def movePacman(canvas, direction):
 
 
 def main():
+    grid = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0],
+        [0, 1, 0, 0, 0, 1, 0],
+        [0, 1, 0, 2, 0, 1, 0],
+        [0, 1, 1, 1, 0, 1, 0],
+        [0, 1, 0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ]
+    pacmanPath = [[6, 1], [5, 1], [4, 1], [3, 1], [2, 1], [1, 1],
+                  [1, 2], [1, 3], [1, 4], [1, 5],
+                  [2, 5], [3, 5], [4, 5], [5, 5], [6, 5],
+                  [6, 4], [6, 3], [6, 2],
+                  [4, 2], [4, 3], [3, 3]]
+
     gui = Tk()
-    screen = Board()
+    screen = Board(grid, pacmanPath)
     gui.mainloop()
 
 
